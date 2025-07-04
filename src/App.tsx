d3b759a77e518822f52f1a56465f1195bf709292
Shyam <shyam@Shyams-MacBook-Pro.local>
2025-07-04 21:54:49 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-6 text-2xl">
+  Tailwind is Working 🎉
+</div>
+
+    </>
+  )
+}
+
+export default App
